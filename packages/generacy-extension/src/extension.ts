03b566fc094f08e4a421fa5c1aff50ddc915cb09
christrudelpw <chris@generacy.ai>
2026-01-21 21:26:13 +0000

    chore: Commit pending work on #50
    Ref: #50

--- packages/generacy-extension/src/extension.ts
+++ packages/generacy-extension/src/extension.ts
@@ -1,6 +1,7 @@
 import * as vscode from 'vscode';
-import { EXTENSION_ID, COMMANDS } from './constants';
-import { getConfig, getLogger, getTelemetry, withErrorHandling, ErrorCode, GeneracyError } from './utils';
+import { EXTENSION_ID } from './constants';
+import { getConfig, getLogger, getTelemetry } from './utils';
+import { registerCommands } from './commands';
 
 /**
  * Called when the extension is activated.
@@ -52,90 +53,6 @@ export function deactivate(): void {
   logger.dispose();
 }
 
-/**
- * Register all extension commands
- */
-function registerCommands(context: vscode.ExtensionContext): void {
-  const logger = getLogger();
-  const telemetry = getTelemetry();
-
-  const commands: Array<{ id: string; handler: () => void | Promise<void> }> = [
-    {
-      id: COMMANDS.createWorkflow,
-      handler: withErrorHandling(handleCreateWorkflow, { showOutput: true }),
-    },
-    {
-      id: COMMANDS.runWorkflow,
-      handler: withErrorHandling(handleRunWorkflow, { showOutput: true }),
-    },
-    {
-      id: COMMANDS.debugWorkflow,
-      handler: withErrorHandling(handleDebugWorkflow, { showOutput: true }),
-    },
-    {
-      id: COMMANDS.validateWorkflow,
-      handler: withErrorHandling(handleValidateWorkflow, { showOutput: true }),
-    },
-    {
-      id: COMMANDS.refreshExplorer,
-      handler: handleRefreshExplorer,
-    },
-  ];
-
-  for (const { id, handler } of commands) {
-    const wrappedHandler = async () => {
-      const startTime = Date.now();
-      logger.debug(`Executing command: ${id}`);
-      try {
-        await handler();
-        telemetry.trackCommand(id, Date.now() - startTime);
-      } catch (error) {
-        telemetry.trackError(
-          error instanceof GeneracyError ? error.code : ErrorCode.Unknown,
-          error instanceof Error ? error.message : String(error)
-        );
-        throw error;
-      }
-    };
-
-    const disposable = vscode.commands.registerCommand(id, wrappedHandler);
-    context.subscriptions.push(disposable);
-    logger.debug(`Registered command: ${id}`);
-  }
-}
-
-/**
- * Command handlers
- */
-async function handleCreateWorkflow(): Promise<void> {
-  const logger = getLogger();
-  logger.info('Command: Create Workflow');
-  await vscode.window.showInformationMessage('Create Workflow - Not yet implemented');
-}
-
-async function handleRunWorkflow(): Promise<void> {
-  const logger = getLogger();
-  logger.info('Command: Run Workflow');
-  await vscode.window.showInformationMessage('Run Workflow - Not yet implemented');
-}
-
-async function handleDebugWorkflow(): Promise<void> {
-  const logger = getLogger();
-  logger.info('Command: Debug Workflow');
-  await vscode.window.showInformationMessage('Debug Workflow - Not yet implemented');
-}
-
-async function handleValidateWorkflow(): Promise<void> {
-  const logger = getLogger();
-  logger.info('Command: Validate Workflow');
-  await vscode.window.showInformationMessage('Validate Workflow - Not yet implemented');
-}
-
-function handleRefreshExplorer(): void {
-  const logger = getLogger();
-  logger.info('Command: Refresh Explorer');
-  // Tree view refresh will be implemented in a future task
-}
 
 /**
  * Get the extension version from package.json
